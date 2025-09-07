901df84e5dc89e6055804d011576b70e7323f72c
Babapour <m.babapour@Behsacorp.com>
2025-09-07 08:52:34 +0330

    add resposive 2xl

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -38,16 +38,16 @@ function Community() {
       }`}
     >
       <img src={mountain} className="w-full rounded-lg" />
-      <p className="absolute top-28 left-1/2 bg-opacity-60 text-emerald-500 transform -translate-x-1/2 -translate-y-1/2 px-3 py-1 rounded-lg">
+      <p className="absolute top-28 left-1/2 bg-opacity-60 text-emerald-500 transform -translate-x-1/2 -translate-y-1/2 px-3 py-1 rounded-lg 2xl:top-48">
         A PART OF STORMSEEK COMMUNITY
       </p>
-      <p className="absolute top-44 left-1/2 bg-opacity-60 text-white transform -translate-x-1/2 -translate-y-1/2 px-3 py-1 rounded-lg text-4xl">
+      <p className="absolute top-44 left-1/2 bg-opacity-60 text-white transform -translate-x-1/2 -translate-y-1/2 px-3 py-1 rounded-lg text-4xl 2xl:top-60">
         Report a storm you encounter
       </p>
-      <div className="absolute top-72 left-2/5 transform -translate-x-1/2 -translate-y-1/3">
+      <div className="absolute top-72 left-2/5 transform -translate-x-1/2 -translate-y-1/3 2xl:top-96 2xl:left-5/12 2xl:ml-3">
         <ReportCard />
       </div>
-      <p className="absolute top-4/5 left-1/2 bg-opacity-60 transform -translate-x-1/2 -translate-y-1/2 px-3 py-1 rounded-lg mt-5 text-xs">
+      <p className="absolute top-4/5 left-1/2 bg-opacity-60 transform -translate-x-1/2 -translate-y-1/2 px-3 py-1 rounded-lg mt-5 text-xs 2xl:top-7/11 2xl:z-40">
         every report gives you 10 points
       </p>
     </div>
--- src/components/OverlayPic.jsx
+++ src/components/OverlayPic.jsx
@@ -3,13 +3,13 @@ import Phone from "../assets/iphone_16_-_free.glb";
 
 function OverlayPic() {
   return (
-    <div className="absolute top-4/12 left-3/4 -translate-x-3/5 z-10 pointer-events-none">
+    <div className="absolute top-4/12 left-3/4 -translate-x-3/5 z-10 pointer-events-none 2xl:top-4/12">
       <div className="w-[300px] md:w-[400px]">
         <model-viewer
           src={Phone}
           alt="iPhone 16 3D Model"
           style={{ width: "100%", height: "500px" }}
-          className="scale-125"
+          className="scale-125 2xl:scale-110"
         ></model-viewer>
       </div>
     </div>
